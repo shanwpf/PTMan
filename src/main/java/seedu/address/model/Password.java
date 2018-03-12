97bb2e20bf2ac95e892d8692d4057daf9681b59d
koo1993 <chinchyekoo@hotmail.com>
2018-03-12 19:24:46 +0800

    Add password class and test case for password

---
+++ src/main/java/seedu/address/model/Password.java
@@ -0,0 +1,116 @@
+package seedu.address.model;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.ptman.commons.util.AppUtil.checkArgument;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
+/**
+ * Represents a Password in PartTimeManger
+ *
+ */
+public class Password {
+    public static final String MESSAGE_PASSWORD_CONSTRAINTS =
+            "Password should be at least 8 character and no spaces.";
+
+    public static final String DEFAULT_PASSWORD =
+            "DEFAULT1";
+
+    /**
+     * accept all password that do not have whitespaces and at least 8 characters.
+     */
+    public static final String PASSWORD_VALIDATION_REGEX = "^(?=\\S+$).{8,}$";
+
+    private String passwordHash;
+
+    /**
+     * constructor for default password
+     */
+    public Password() {
+        passwordHash = generatePasswordHash(DEFAULT_PASSWORD);
+    }
+
+    /**
+     * Constructor to assign a password
+     * @param password
+     */
+    public Password(String password) {
+        requireNonNull(password);
+        checkArgument(isValidPassword(password), MESSAGE_PASSWORD_CONSTRAINTS);
+        passwordHash = generatePasswordHash(password);
+    }
+
+    /**
+     * Only use this method for loading password file,
+     * given that you know the encoded hash.
+     * @param encodedHash
+     */
+    public void changeHash(String encodedHash) {
+        passwordHash = encodedHash;
+    }
+
+    /**
+     *
+     * @param test
+     * @return true if password is of correct format
+     */
+    public static boolean isValidPassword(String test) {
+        return test.matches(PASSWORD_VALIDATION_REGEX);
+    }
+
+    /**
+     * check if passwordHash generated from the string is same as current passwordHash
+     * @param password
+     * @return true if same
+     */
+    public boolean isCorrectPassword(String password) {
+        if (passwordHash.equals(generatePasswordHash(password))) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Change password given a password
+     * @return true if password is changed
+     */
+    public boolean checkAndChangePassword(String oldPassword, String newPassword) {
+        if (isCorrectPassword(oldPassword)) {
+            this.passwordHash = generatePasswordHash(newPassword);
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Generate passwordHash given a string password
+     * @param password
+     * @return passwordHash in String
+     */
+    public String generatePasswordHash(String password) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] byteHash = digest.digest(password.getBytes(StandardCharsets.UTF_8));
+            String encodedHash = Base64.getEncoder().encodeToString(byteHash);
+            return encodedHash;
+        } catch (NoSuchAlgorithmException noSuchAlgoException) {
+            System.out.println("cannot generate hash: MessageDigest.getInstance");
+        }
+        return null;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Password // instanceof handles nulls
+                && this.passwordHash.equals(((Password) other).passwordHash)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return passwordHash.hashCode();
+    }
+}
---
+++ src/test/java/seedu/address/model/PasswordTest.java
@@ -0,0 +1,59 @@
+package seedu.address.model;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import seedu.ptman.testutil.Assert;
+
+public class PasswordTest {
+    @Test
+    public void constructor_invalidPassword_throwsIllegalArgumentException() {
+        String invalidName = "have space";
+        Assert.assertThrows(IllegalArgumentException.class, () -> new Password(invalidName));
+    }
+
+    @Test
+    public void isValidPassword() {
+        // invalid Password
+        assertFalse(Password.isValidPassword("")); // empty string
+        assertFalse(Password.isValidPassword(" ")); // spaces only
+        assertFalse(Password.isValidPassword("!#@! fdsafdsafdsa")); // characters with space
+        assertFalse(Password.isValidPassword("less8")); // less than 8 character
+
+        // valid name
+        assertTrue(Password.isValidPassword("peterjack")); // alphabets only
+        assertTrue(Password.isValidPassword("1234567dsa6")); // numbers only
+        assertTrue(Password.isValidPassword("peterthe2nd")); // alphanumeric characters
+        assertTrue(Password.isValidPassword("CapitalTan")); // with capital letters
+        assertTrue(Password.isValidPassword("Da@acks$RayJr2nd")); // alphanumeric with captial and special character
+    }
+
+    @Test
+    public void isCorrectPassword() {
+        Password password = new Password("ThisIsThePassword");
+
+        assertFalse(password.isCorrectPassword("thisiswrongpassword")); //wrong password
+        assertFalse(password.isCorrectPassword("ThisIsThepassword")); // wrong password
+
+        assertTrue(password.isCorrectPassword("ThisIsThePassword")); //correct password
+    }
+
+
+    @Test
+    public void changePassword() {
+
+        Password password = new Password("ThisIsThePassword");
+        Password expectedPassword = new Password("newPassword");
+
+        assertFalse(password.checkAndChangePassword("this is the password", "newPassword")); //wrong password
+        assertFalse(password.checkAndChangePassword("notapassword", "newPassword")); //wrong password
+
+        assertTrue(password.checkAndChangePassword("ThisIsThePassword", "newPassword")); //wrong password
+        assertEquals(password, expectedPassword);
+
+    }
+
+}
