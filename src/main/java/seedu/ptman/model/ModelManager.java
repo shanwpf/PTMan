d5f00ebbd5749536c3d280f675a60876789dc734
Bangjie <qq315035253@gmail.com>
2018-04-15 17:04:14 +0800

    fix undo bug

--- src/main/java/seedu/ptman/model/ModelManager.java
+++ src/main/java/seedu/ptman/model/ModelManager.java
@@ -77,6 +77,7 @@ public class ModelManager extends ComponentManager implements Model {
     public void resetData(ReadOnlyPartTimeManager newData) {
         partTimeManager.resetData(newData);
         indicatePartTimeManagerChanged();
+        indicateOutletInformationChanged();
     }
 
     @Override
